d0cad048e518ce1e009ad3998824a192fae30047
gunjana <gunjan.agrawal@technonjr.org>
2024-04-20 22:19:28 +0530

    Add files via upload

---
+++ src/CitiesDataListBuilder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\migration_to_drupal;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+
+/**
+ * Provides a list controller for the cities data entity type.
+ */
+class CitiesDataListBuilder extends EntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header['id'] = $this->t('No.');
+    $header['city'] = $this->t('City Name');
+    $header['pop'] = $this->t('Pop');
+    $header['state'] = $this->t('State');
+    $header['location'] = $this->t('Location');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    $row['id'] = $entity->id();
+    $row['city'] = $entity->get('city')->getValue()[0]["value"];
+    $row['pop'] = $entity->get('pop')->getValue()[0]["value"];
+    $row['state'] = $entity->get('state')->getValue()[0]["value"];
+    $row['location'] = $entity->get('location')->getValue()[0]["value"];
+    return $row + parent::buildRow($entity);
+  }
+
+}
