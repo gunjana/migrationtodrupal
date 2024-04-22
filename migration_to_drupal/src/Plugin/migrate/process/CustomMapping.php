16203ca256c6fb67cae4058ef508d21a33f83838
Gunjan Agrawal <gunjanagrawal085@gmail.com>
2024-04-22 16:46:41 +0530

    Added Readme file.

--- migration_to_drupal/src/Plugin/migrate/process/CustomMapping.php
+++ migration_to_drupal/src/Plugin/migrate/process/CustomMapping.php
@@ -19,9 +19,7 @@ class CustomMapping extends ProcessPluginBase {
    * Maps Json attributes to custom entity attribute.
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $mapping_config = \Drupal::config('migration_to_drupal.mapping')->getRawData();
-    dump($mapping_config);
-    die();
+    $mapping_config = \Drupal::config('migration_to_drupal.mapping');
     if (isset($mapping_config[$destination_property]['source_key'])) {
       $source_key = $mapping_config[$destination_property]['source_key'];
       return $row->getSourceProperty($source_key);
